8caf7020af73b0b9a6ac19be37fce0a5cd7627d1
GuilhermeMarcon <guismarcon@hotmail.com>
2017-05-29 14:35:13 -0300

    Organizado o esqueleto
    Esqueleto pronto para receber as funções dos sensores físicos e virtuais.

--- client.c
+++ client.c
@@ -1,60 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <time.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
+#include <netdb.h>
 
-void error(const char *msg)
-{
+#define ALTITUDE    "cmd alt"
+#define LONGITUDE   "cmd long"
+#define LATITUDE    "cmd lat"
+#define VELOCIDADE  "cmd vel"
+#define ORIENTACAO  "cmd ori"
+#define TEMPO_VOO   "cmd time"
+#define PESO        "cmd peso"
+
+void error(const char *msg){
     perror(msg);
     exit(0);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
-    char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
+    char msg[256];
+    if (argc < 3){
+        fprintf(stderr,"usage %s hostname port\n", argv[0]);
+        exit(0);
     }
+
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
+    if (sockfd < 0) error("ERROR opening socket");
+
     server = gethostbyname(argv[1]);
-    if (server == NULL) {
+    if (server == NULL){
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
+
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
-    
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) error("ERROR connecting");
+    //conectado
+
 	while(1){
-	printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
+        //recebendo o comando
+        bzero(msg,256);
+        n = read(sockfd,msg,255);
+        if (n < 0) error("ERROR reading from socket");
+
+        //TODO decidir a resposta
+        bzero(msg,256);
+
+        //enviar a resposta
+        n = write(sockfd,msg,strlen(msg));
+        if (n < 0) error("ERROR writing to socket");
 	}
-   close(sockfd);
+
+    close(sockfd);
     return 0;
 }
--- server.c
+++ server.c
@@ -3,10 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+
+#define ALTITUDE    "cmd alt"
+#define LONGITUDE   "cmd long"
+#define LATITUDE    "cmd lat"
+#define VELOCIDADE  "cmd vel"
+#define ORIENTACAO  "cmd ori"
+#define TEMPO_VOO   "cmd time"
+#define PESO        "cmd peso"
 
 void error(const char *msg)
 {
@@ -16,42 +25,49 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno;
-     socklen_t clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, 
-                 (struct sockaddr *) &cli_addr, 
-                 &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-     while(1){
-     bzero(buffer,256);
-		n = read(newsockfd,buffer,255);
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    char msg[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+
+    if (argc < 2) {
+        fprintf(stderr,"ERROR, no port provided\n");
+        exit(1);
+    }
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) error("ERROR opening socket");
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) error("ERROR on binding");
+
+    listen(sockfd,5);
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    if (newsockfd < 0) error("ERROR on accept");
+
+    while(1){
+        //lendo o comando
+        bzero(msg,256);
+        fgets(msg,255,stdin);
+        if(strcmp(msg, "exit") == 0) break;
+
+        //enviando o comando
+        n = write(newsockfd, msg, 255);
+        if (n < 0) error("ERROR writing to socket");
+
+        //lendo a resposta
+		n = read(newsockfd,msg,255);
 		if (n < 0) error("ERROR reading from socket");
-		printf("Here is the message: %s\n",buffer);
-		n = write(newsockfd,"I got your message",18);
-		if (n < 0) error("ERROR writing to socket");
-	}
+
+		//TODO fazer as operacoes de sensores virtuais
+    }
      close(newsockfd);
      close(sockfd);
-     return 0; 
+     return 0;
 }
