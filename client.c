808378f400cf1104e269a5e603bad1d144a98b40
GuilhermeCFernandes <guilherme.correa.fernandes255@gmail.com>
2017-06-01 20:33:27 -0300

    Contando tempo de voo

--- client.c
+++ client.c
@@ -7,14 +7,15 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <time.h>
 
-#define CMD_ALTITUDE    "cmd alt\n"
+#define CMD_ALTITUDE	"cmd alt\n"
 #define CMD_LONGITUDE   "cmd long\n"
-#define CMD_LATITUDE    "cmd lat\n"
+#define CMD_LATITUDE	"cmd lat\n"
 #define CMD_VELOCIDADE  "cmd vel\n"
 #define CMD_ORIENTACAO  "cmd ori\n"
 #define CMD_TEMPO_VOO   "cmd time\n"
-#define CMD_PESO        "cmd peso\n"
+#define CMD_PESO	"cmd peso\n"
 
 #define MSG_SIZE 256
 double fAltitude(double t);
@@ -25,84 +26,92 @@ void   fOrientacao(double* x, double* y, double* z, double t);
 double fPeso(double t);
 
 void error(const char *msg){
-    perror(msg);
-    exit(0);
+	perror(msg);
+	exit(0);
 }
 
 int main(int argc, char *argv[]){
-    srand(time(NULL));
+	srand(time(NULL));
 
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
+	int sockfd, portno, n;
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
+	
+	time_t seconds;
 
-    char msg[MSG_SIZE];
-    if (argc < 3){
-        fprintf(stderr,"usage %s hostname port\n", argv[0]);
-        exit(0);
-    }
-
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) error("ERROR opening socket");
-
-    server = gethostbyname(argv[1]);
-    if (server == NULL){
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
-
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) error("ERROR connecting");
-    //conectado
-
-    clock_t beginning = clock();
-
-	while(1){
-        //recebendo o comando
-        bzero(msg,MSG_SIZE);
-        n = read(sockfd,msg,255);
-        if (n < 0) error("ERROR reading from socket");
-
-        if(strcmp(msg, "exit\n") == 0) break;
-        else if(strcmp(CMD_ALTITUDE, msg) == 0){
-            bzero(msg,MSG_SIZE);
-            snprintf(msg, MSG_SIZE, "%lf", fAltitude((double)(clock()-beginning)/CLOCKS_PER_SEC));
-        }
-        else if(strcmp(CMD_LONGITUDE, msg) == 0){
-
-        }
-        else if(strcmp(CMD_LATITUDE, msg) == 0){
-
-        }
-        else if(strcmp(CMD_VELOCIDADE, msg) == 0){
-
-        }
-        else if(strcmp(CMD_ORIENTACAO, msg) == 0){
-
-        }
-        else if(strcmp(CMD_TEMPO_VOO, msg) == 0){
-
-        }
-        else if(strcmp(CMD_PESO, msg) == 0){
-
-        }
-
-        //enviar a resposta
-        n = write(sockfd,msg,strlen(msg));
-        if (n < 0) error("ERROR writing to socket");
+	char msg[MSG_SIZE];
+	if (argc < 3){
+		fprintf(stderr,"usage %s hostname port\n", argv[0]);
+		exit(0);
 	}
 
-    close(sockfd);
-    return 0;
+	portno = atoi(argv[2]);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) error("ERROR opening socket");
+
+	server = gethostbyname(argv[1]);
+	if (server == NULL){
+		fprintf(stderr,"ERROR, no such host\n");
+		exit(0);
+	}
+
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+	serv_addr.sin_port = htons(portno);
+	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) error("ERROR connecting");
+	//conectado
+	clock_t beginning = time(NULL);
+	clock_t tempoVoo;
+
+	while(1){
+	//recebendo o comando
+	bzero(msg,MSG_SIZE);
+	n = read(sockfd,msg,255);
+	if (n < 0) error("ERROR reading from socket");
+
+	if(strcmp(msg, "exit\n") == 0) break;
+	else if(strcmp(CMD_ALTITUDE, msg) == 0){
+		bzero(msg,MSG_SIZE);
+		snprintf(msg, MSG_SIZE, "%lf", fAltitude((double)(time(NULL)-beginning)/CLOCKS_PER_SEC));
+	}
+	else if(strcmp(CMD_LONGITUDE, msg) == 0){
+		
+	}
+	else if(strcmp(CMD_LATITUDE, msg) == 0){
+
+	}
+	else if(strcmp(CMD_VELOCIDADE, msg) == 0){
+
+	}
+	else if(strcmp(CMD_ORIENTACAO, msg) == 0){
+
+	}
+	else if(strcmp(CMD_TEMPO_VOO, msg) == 0){
+		bzero(msg,MSG_SIZE);
+		tempoVoo = (time(NULL) - beginning)/60;
+		snprintf(msg, MSG_SIZE, "%ld minutes", tempoVoo);
+	}
+	else if(strcmp(CMD_PESO, msg) == 0){
+
+	}
+	else{
+		bzero(msg,MSG_SIZE);
+		snprintf(msg, MSG_SIZE, "invalid command");
+	}
+
+	//enviar a resposta
+	n = write(sockfd,msg,strlen(msg));
+	if (n < 0) error("ERROR writing to socket");
+	}
+
+	close(sockfd);
+	return 0;
 }
 
 double fAltitude(double t){
-    if(t < 4*7200) return t; //da pra mudar pra uma parabola e tals, mas whatever
-    else if(t < 8*7200) return 11000;
-    else return (t < 11000 ? 11000+8*7200-t : 0);
+	if(t < 4*7200) return t; //da pra mudar pra uma parabola e tals, mas whatever
+	else if(t < 8*7200) return 11000;
+	else return (t < 11000 ? 11000+8*7200-t : 0);
 }
 
--- server.c
+++ server.c
@@ -9,71 +9,71 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-#define CMD_ALTITUDE    "cmd alt\n"
+#define CMD_ALTITUDE	"cmd alt\n"
 #define CMD_LONGITUDE   "cmd long\n"
-#define CMD_LATITUDE    "cmd lat\n"
+#define CMD_LATITUDE	"cmd lat\n"
 #define CMD_VELOCIDADE  "cmd vel\n"
 #define CMD_ORIENTACAO  "cmd ori\n"
 #define CMD_TEMPO_VOO   "cmd time\n"
-#define CMD_PESO        "cmd peso\n"
-#define CMD_DIST        "cmd dist\n"
+#define CMD_PESO	"cmd peso\n"
+#define CMD_DIST	"cmd dist\n"
 
 #define MSG_SIZE 256
 
 void error(const char *msg){
-    perror(msg);
-    exit(1);
+	perror(msg);
+	exit(1);
 }
 
 int main(int argc, char *argv[]){
-    srand(time(NULL));
+	srand(time(NULL));
 
-    int sockfd, newsockfd, portno;
-    socklen_t clilen;
-    char msg[MSG_SIZE];
-    struct sockaddr_in serv_addr, cli_addr;
-    int n;
+	int sockfd, newsockfd, portno;
+	socklen_t clilen;
+	char msg[MSG_SIZE];
+	struct sockaddr_in serv_addr, cli_addr;
+	int n;
 
-    if (argc < 2) {
-        fprintf(stderr,"ERROR, no port provided\n");
-        exit(1);
-    }
+	if (argc < 2) {
+	fprintf(stderr,"ERROR, no port provided\n");
+	exit(1);
+	}
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) error("ERROR opening socket");
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) error("ERROR opening socket");
 
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    portno = atoi(argv[1]);
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portno);
-    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) error("ERROR on binding");
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	portno = atoi(argv[1]);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) error("ERROR on binding");
 
-    listen(sockfd,5);
-    clilen = sizeof(cli_addr);
-    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-    if (newsockfd < 0) error("ERROR on accept");
+	listen(sockfd,5);
+	clilen = sizeof(cli_addr);
+	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+	if (newsockfd < 0) error("ERROR on accept");
 
-    while(1){
-        //lendo o comando
-        bzero(msg,MSG_SIZE);
-        fgets(msg,255,stdin);
+	while(1){
+	//lendo o comando
+	bzero(msg,MSG_SIZE);
+	fgets(msg,255,stdin);
 
-        //enviando o comando
-        n = write(newsockfd, msg, 255);
-        if (n < 0) error("ERROR writing to socket");
+	//enviando o comando
+	n = write(newsockfd, msg, 255);
+	if (n < 0) error("ERROR writing to socket");
 
-        if(strcmp(msg, "exit\n") == 0) break;
+	if(strcmp(msg, "exit\n") == 0) break;
 
-        //lendo a resposta
+	//lendo a resposta
 		n = read(newsockfd,msg,255);
 		if (n < 0) error("ERROR reading from socket");
 
 		//TODO fazer as operacoes de sensores virtuais
 		printf("%s\n", msg);
-    }
-    close(newsockfd);
-    close(sockfd);
-    return 0;
+	}
+	close(newsockfd);
+	close(sockfd);
+	return 0;
 }
 
